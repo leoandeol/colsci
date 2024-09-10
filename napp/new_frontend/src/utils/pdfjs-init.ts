6c2fa66af9ca3de5e023f5c6b397f1a765f1c25e
Léo Andéol <leo@andeol.eu>
2024-09-10 11:54:17 +0200

    Added local link to worker to avoid network issues

--- napp/new_frontend/src/utils/pdfjs-init.ts
+++ napp/new_frontend/src/utils/pdfjs-init.ts
@@ -1,6 +1,13 @@
+// import * as pdfjsLib from 'pdfjs-dist';
+
+// // Set the worker source to match the version used by PdfLoader
+// pdfjsLib.GlobalWorkerOptions.workerSrc = "https://unpkg.com/pdfjs-dist@4.4.168/build/pdf.worker.min.mjs";
+
+// export { pdfjsLib };
+
 import * as pdfjsLib from 'pdfjs-dist';
 
-// Set the worker source to match the version used by PdfLoader
-pdfjsLib.GlobalWorkerOptions.workerSrc = "https://unpkg.com/pdfjs-dist@4.4.168/build/pdf.worker.min.mjs";
+// Définir le chemin vers le worker local .mjs
+pdfjsLib.GlobalWorkerOptions.workerSrc = './node_modules/pdfjs-dist/build/pdf.worker.min.mjs';
 
 export { pdfjsLib };
